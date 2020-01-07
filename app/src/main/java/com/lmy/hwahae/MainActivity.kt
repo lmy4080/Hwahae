2cef9883d0dfeb918d167b4cefe9df25c23c19e6
mingyoung <tkfkd1212!>
2020-01-08 01:24:28 +0900

    added toolbar/header and searchbar at the top

--- app/src/main/java/com/lmy/hwahae/MainActivity.kt
+++ app/src/main/java/com/lmy/hwahae/MainActivity.kt
@@ -2,11 +2,47 @@ package com.lmy.hwahae
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.lmy.hwahae.common.SkinTypes
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initSpinner()
+    }
+
+    private fun initSpinner() {
+
+        val spinnerAdapter = ArrayAdapter(this, R.layout.main_spinner_main_item, SkinTypes.getAllSkinTypes())
+        spinnerAdapter.setDropDownViewResource(R.layout.main_spinner_dropdown_item)
+        sp_skin_type.adapter = spinnerAdapter
+
+        sp_skin_type.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+                //아이템이 클릭 되면 맨 위부터 position 0번부터 순서대로 동작하게 됩니다.
+                when(position) {
+                    0   ->  {
+                    }
+                    1   ->  {
+                    }
+                    2   -> {
+                    }
+                    3   -> {
+                    }
+                    //...
+                    else -> {
+                    }
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
     }
 }
