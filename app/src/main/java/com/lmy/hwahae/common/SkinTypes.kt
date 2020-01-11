249af3aa1d5d9c4397f88b25ddc5ec4c787a88fb
mingyoung <tkfkd1212!>
2020-01-11 21:03:21 +0900

    changed SkinTypes.kt class to SkinTypes.kt object

--- app/src/main/java/com/lmy/hwahae/common/SkinTypes.kt
+++ app/src/main/java/com/lmy/hwahae/common/SkinTypes.kt
@@ -1,8 +1,6 @@
 package com.lmy.hwahae.common
 
-class SkinTypes {
-    companion object {
-        private var allSkinTypes: Array<String> = arrayOf("모든 피부 타입", "Oily", "Dry", "Sensitive")
-        fun getAllSkinTypes() = allSkinTypes
-    }
+object SkinTypes {
+    private var allSkinTypes: Array<String> = arrayOf("모든 피부 타입", "Oily", "Dry", "Sensitive")
+    fun getAllSkinTypes() = allSkinTypes
 }
