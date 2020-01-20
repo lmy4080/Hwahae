8c264dbd1dbe91fc12cd6e517b931f6230bbab37
mingyoung <tkfkd1212!>
2020-01-20 23:32:16 +0900

    created the retrofit service based on server base-parameters

--- app/src/main/java/com/lmy/hwahae/datasoruce/api/HwahaeWebService.kt
+++ app/src/main/java/com/lmy/hwahae/datasoruce/api/HwahaeWebService.kt
@@ -1,8 +1,12 @@
 package com.lmy.hwahae.datasoruce.api
 
 import com.lmy.hwahae.datasoruce.model.IndexViewModel
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface HwahaeWebService {
 
@@ -16,4 +20,28 @@ interface HwahaeWebService {
         @Query("search") search: String?
     ) : IndexViewModel
 
+    /**
+     * Create retrofit-service for backend-api server
+     */
+    companion object RetrofitBuilder {
+
+        /* Server Base-Parameters */
+        private const val BASE_URL = "https://6uqljnm1pb.execute-api.ap-northeast-2.amazonaws.com/prod/"
+        private const val CONNECT_TIMEOUT = 15L
+        private const val READ_TIMEOUT = 3L
+        private const val WRITE_TIMEOUT = 3L
+
+        private val client = OkHttpClient.Builder()
+            .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+
+        val service: HwahaeWebService = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HwahaeWebService::class.java)
+    }
 }
