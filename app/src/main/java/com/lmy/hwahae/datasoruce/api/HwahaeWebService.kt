244ec385c7c9541d4eaf273f282ee7bf8fd012b8
mingyoung <tkfkd1212!>
2020-01-20 23:28:55 +0900

    defined the api query for product-list on IndexView

--- app/src/main/java/com/lmy/hwahae/datasoruce/api/HwahaeWebService.kt
+++ app/src/main/java/com/lmy/hwahae/datasoruce/api/HwahaeWebService.kt
@@ -1,4 +1,19 @@
 package com.lmy.hwahae.datasoruce.api
 
+import com.lmy.hwahae.datasoruce.model.IndexViewModel
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface HwahaeWebService {
+
+    /**
+     * GET request to backend-api server
+     */
+    @GET("products")
+    suspend fun getProductList(
+        @Query("skin_type") skin_type: String,
+        @Query("page") page: Int,
+        @Query("search") search: String?
+    ) : IndexViewModel
+
 }
