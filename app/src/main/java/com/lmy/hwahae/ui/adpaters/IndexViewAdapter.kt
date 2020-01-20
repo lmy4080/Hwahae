ea3b3ad84d8cdbd4bab746397a68ec3cebcff117
mingyoung <tkfkd1212!>
2020-01-21 02:13:35 +0900

    added the adapter extending 'PagedListAdapter' to store the pagedList data

---
+++ app/src/main/java/com/lmy/hwahae/ui/adpaters/IndexViewAdapter.kt
@@ -0,0 +1,70 @@
+package com.lmy.hwahae.ui.adpaters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.net.toUri
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.lmy.hwahae.R
+import com.lmy.hwahae.datasoruce.model.IndexViewProduct
+import java.text.NumberFormat
+import java.util.*
+
+class IndexViewAdapter: PagedListAdapter<IndexViewProduct, IndexViewAdapter.ItemViewHolder>(DIFF_CALLBACK) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val layoutView: LinearLayout = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false) as LinearLayout
+        resizeViewHolder(layoutView, parent)
+        return ItemViewHolder(layoutView)
+    }
+
+    /**
+     * Resize ViewHolder's height
+     * 2.5 items per page in row
+     */
+    private fun resizeViewHolder(layoutView: LinearLayout, parent: ViewGroup) {
+        val layoutViewHeight = parent.findViewById<RecyclerView>(R.id.rv_product).layoutManager?.height
+        layoutView.layoutParams.height = (layoutViewHeight!! / 2.5).toInt()
+        layoutView.findViewById<ImageView>(R.id.iv_thumbnail).layoutParams.height = (layoutView.layoutParams.height / 1.5).toInt()
+    }
+
+    /**
+     * Bind data to viewHolder
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) { holder.bind(getItem(position)) }
+
+    /**
+     * ItemViewHolder
+     */
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(product: IndexViewProduct?) {
+            itemView.tv_title.text = product?.title
+            itemView.tv_price.text = formatPrice(product?.price)
+            Glide.with(itemView)
+                .asBitmap()
+                .load(product?.thumbnail_image?.toUri())
+                .into(itemView.iv_thumbnail)
+        }
+
+        /* Format, ex) 10000 -> 10,000원 */
+        private fun formatPrice(price: String?): String =
+            NumberFormat.getNumberInstance(Locale.KOREA).format(price?.toInt()) + "원"
+
+    }
+
+    /**
+     * Compare to decide whether data should be fetched or not
+     */
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<IndexViewProduct>() {
+            override fun areItemsTheSame(oldItem: IndexViewProduct, newItem: IndexViewProduct): Boolean = oldItem.id == newItem.id
+            override fun areContentsTheSame(oldItem: IndexViewProduct, newItem: IndexViewProduct): Boolean = oldItem == newItem
+        }
+    }
+}
