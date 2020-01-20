04492c083660c20a9072b6a99c3c559874ef4f6b
mingyoung <tkfkd1212!>
2020-01-21 00:19:09 +0900

    defined 'loadInitial' and 'loadAfter' funcs

--- app/src/main/java/com/lmy/hwahae/datasoruce/HwahaeDataSource.kt
+++ app/src/main/java/com/lmy/hwahae/datasoruce/HwahaeDataSource.kt
@@ -2,11 +2,20 @@ package com.lmy.hwahae.datasoruce
 
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
+import com.lmy.hwahae.datasoruce.api.HwahaeWebService
 import com.lmy.hwahae.datasoruce.api.NetworkState
 import com.lmy.hwahae.datasoruce.model.IndexViewProduct
+import com.lmy.hwahae.ui.status.UiStatus
+import kotlinx.coroutines.*
+import java.net.SocketTimeoutException
 
 class HwahaeDataSource: PageKeyedDataSource<Int, IndexViewProduct>() {
 
+    /**
+     * Initial Page Key
+     */
+    private val INITIAL_PAGE_KEY = 1
+    
     /**
      * Network State
      */
@@ -15,18 +24,83 @@ class HwahaeDataSource: PageKeyedDataSource<Int, IndexViewProduct>() {
         mState.postValue(state)
     }
 
-    override fun loadInitial(
-        params: LoadInitialParams<Int>,
-        callback: LoadInitialCallback<Int, IndexViewProduct>
-    ) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, IndexViewProduct>) {
+
+        /**
+         * Define the coroutine exception handler
+         */
+        val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+
+            when(throwable){
+                /* Socket Time-Out Exception */
+                is SocketTimeoutException -> loadInitial(params,callback)
+
+                /* other Exceptions */
+
+            }
+            throwable.printStackTrace()
+        }
+
+        /**
+         * Fetch data from backend-api server
+         */
+        updateNetworkState(NetworkState.LOADING)
+        CoroutineScope(Dispatchers.IO + coroutineExceptionHandler).launch {
+            try {
+                HwahaeWebService.service.getProductList(UiStatus.currentSkinType, INITIAL_PAGE_KEY, UiStatus.currentSearchQuery).apply {
+                    withContext(Dispatchers.Main) {
+                        callback.onResult(this@apply.body, 0, this@apply.body.size, null, INITIAL_PAGE_KEY+1)
+                        updateNetworkState(NetworkState.DONE)
+                    }
+                }
+            }
+            catch(throwable: Throwable) {
+                withContext(Dispatchers.Main) {
+                    updateNetworkState(NetworkState.FAILED)
+                }
+                throw throwable
+            }
+        }
     }
 
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, IndexViewProduct>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        /**
+         * Define the coroutine exception handler
+         */
+        val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+
+            when(throwable){
+                /* Socket Time-Out Exception */
+                is SocketTimeoutException -> loadAfter(params,callback)
+
+                /* other Exceptions */
+
+            }
+            throwable.printStackTrace()
+        }
+
+        /**
+         * Fetch data from backend-api server
+         */
+        updateNetworkState(NetworkState.LOADING)
+        CoroutineScope(Dispatchers.IO + coroutineExceptionHandler).launch {
+            try {
+                HwahaeWebService.service.getProductList(UiStatus.currentSkinType, params.key, UiStatus.currentSearchQuery).apply {
+                    withContext(Dispatchers.Main) {
+                        callback.onResult(this@apply.body, params.key+1)
+                        updateNetworkState(NetworkState.DONE)
+                    }
+                }
+            }
+            catch(throwable: Throwable) {
+                withContext(Dispatchers.Main) {
+                    updateNetworkState(NetworkState.FAILED)
+                }
+                throw throwable
+            }
+        }
     }
 
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, IndexViewProduct>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, IndexViewProduct>) { /* Do nothing */ }
 }
