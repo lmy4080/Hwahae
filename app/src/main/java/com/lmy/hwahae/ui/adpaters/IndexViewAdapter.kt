3b522eb1ee9e20750b55c44787e6bd285f190787
mingyoung <tkfkd1212!>
2020-01-22 07:13:45 +0900

    implemented the items to be shown differently according to orientation

--- app/src/main/java/com/lmy/hwahae/ui/adpaters/IndexViewAdapter.kt
+++ app/src/main/java/com/lmy/hwahae/ui/adpaters/IndexViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.lmy.hwahae.ui.adpaters
 
+import android.content.res.Configuration
 import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
@@ -20,28 +21,20 @@ import java.util.*
 class IndexViewAdapter(onIndexViewAdapterListener: IndexViewAdapterListener): PagedListAdapter<IndexViewProduct, IndexViewAdapter.ItemViewHolder>(DIFF_CALLBACK) {
 
     /**
+     * Save the parent view to resize items according to orientation
+     */
+    private lateinit var parentViewGroup: ViewGroup
+
+    /**
      * Call back when user clicks the product item to check it in detail
      */
     private val onIndexViewAdapterListener: IndexViewAdapterListener = onIndexViewAdapterListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        parentViewGroup = parent
+
         val layoutView: LinearLayout = LayoutInflater.from(parent.context).inflate(R.layout.layout_index_view_items, parent, false) as LinearLayout
-        resizeViewHolder(layoutView, parent)
         return ItemViewHolder(layoutView)
-    }
-
-    /**
-     * Resize ViewHolder's height
-     * 2.5 items per page in row
-     */
-    private fun resizeViewHolder(layoutView: LinearLayout, parent: ViewGroup) {
-
-        var recyclerViewHeight = parent.findViewById<RecyclerView>(R.id.rv_product).layoutManager?.height!!
-        var appBarLayoutHeight = Resources.getSystem().displayMetrics.heightPixels / 15
-
-        recyclerViewHeight += appBarLayoutHeight
-        layoutView.layoutParams.height = (recyclerViewHeight / 2.5).toInt()
-        layoutView.findViewById<ImageView>(R.id.iv_thumbnail).layoutParams.height = (layoutView.layoutParams.height / 1.5).toInt()
     }
 
     /**
@@ -55,6 +48,31 @@ class IndexViewAdapter(onIndexViewAdapterListener: IndexViewAdapterListener): Pa
     inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private var productId: Int? = null
+
+        init {
+            var recyclerViewHeight = parentViewGroup.findViewById<RecyclerView>(R.id.rv_product).layoutManager?.height!!
+            var appBarLayoutHeight = Resources.getSystem().displayMetrics.heightPixels / 15
+            recyclerViewHeight += appBarLayoutHeight
+
+            if(Configuration.ORIENTATION_LANDSCAPE == Resources.getSystem().configuration.orientation) {
+                /**
+                 * Orientation Landscape
+                 * Resize ViewHolder's height
+                 * 1.5 items per page in row
+                 */
+                itemView.layoutParams.height = (recyclerViewHeight / 1.5).toInt()
+                itemView.findViewById<ImageView>(R.id.iv_thumbnail).layoutParams.height = (itemView.layoutParams.height / 1.5).toInt()
+            }
+            else{
+                /**
+                 * Orientation Portrait
+                 * Resize ViewHolder's height
+                 * 2.5 items per page in row
+                 */
+                itemView.layoutParams.height = (recyclerViewHeight / 2.5).toInt()
+                itemView.findViewById<ImageView>(R.id.iv_thumbnail).layoutParams.height = (itemView.layoutParams.height / 1.5).toInt()
+            }
+        }
 
         fun bind(product: IndexViewProduct?) {
             productId = product?.id
