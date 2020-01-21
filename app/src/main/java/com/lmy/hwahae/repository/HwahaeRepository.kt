af0cd0aa416352108cdb3666d112c6c4e4ebc640
mingyoung <tkfkd1212!>
2020-01-22 00:08:08 +0900

    added the network-flow logic during updating the detail of product

--- app/src/main/java/com/lmy/hwahae/repository/HwahaeRepository.kt
+++ app/src/main/java/com/lmy/hwahae/repository/HwahaeRepository.kt
@@ -2,15 +2,13 @@ package com.lmy.hwahae.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
-import com.lmy.hwahae.datasoruce.HwahaeDataSource
 import com.lmy.hwahae.datasoruce.HwahaeDataSourceFactory
 import com.lmy.hwahae.datasoruce.api.HwahaeWebService
-import com.lmy.hwahae.datasoruce.api.NetworkState
 import com.lmy.hwahae.datasoruce.model.DetailViewProduct
 import com.lmy.hwahae.datasoruce.model.IndexViewProduct
+import com.lmy.hwahae.datasoruce.status.NetworkStatus
 import com.lmy.hwahae.ui.status.UiStatus
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -61,7 +59,8 @@ class HwahaeRepository {
     /**
      * Return live-data holding the state of network
      */
-    fun getState(): LiveData<NetworkState> = Transformations.switchMap(mProductDataSourceFactory.mProductListLiveData, HwahaeDataSource::mState)
+    fun getState(): LiveData<NetworkStatus.State> = NetworkStatus.mState
+    //Transformations.switchMap(mProductDataSourceFactory.mProductListLiveData, HwahaeDataSource::mState)
 
     /**
      * Return live-data holding the detail of product
@@ -73,16 +72,19 @@ class HwahaeRepository {
      */
     fun updateProductDetail(productId: Int?) {
 
+        NetworkStatus.updateNetworkState(NetworkStatus.State.LOADING)
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 HwahaeWebService.service.getProductDetail(productId).apply {
                     withContext(Dispatchers.Main) {
                         mProductDetail.setValue(this@apply?.body)
+                        NetworkStatus.updateNetworkState(NetworkStatus.State.DONE)
                     }
                 }
             }
             catch(throwable: Throwable) {
                 throwable.printStackTrace()
+                NetworkStatus.updateNetworkState(NetworkStatus.State.FAILED)
             }
         }
     }
