ba84838f9a077b8afd9ac9b5991fe0c9357bc837
mingyoung <tkfkd1212!>
2020-01-23 02:10:34 +0900

    defined 'DetailViewStatus' to store the data of product detail with the y-position value

---
+++ app/src/main/java/com/lmy/hwahae/ui/status/DetailViewStatus.kt
@@ -0,0 +1,8 @@
+package com.lmy.hwahae.ui.status
+
+import com.lmy.hwahae.datasoruce.remote.model.DetailViewItem
+
+object DetailViewStatus {
+    var detailViewItem: DetailViewItem = DetailViewItem(-1, "", "","", "", -1, -1, -1)
+    var currentPositionY: Int? = 0
+}
