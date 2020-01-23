093195c332ef77d804331b8957083f25cb6465da
mingyoung <tkfkd1212!>
2020-01-24 07:10:39 +0900

    added comments on each state

--- app/src/main/java/com/lmy/hwahae/datasoruce/remote/status/NetworkStatus.kt
+++ app/src/main/java/com/lmy/hwahae/datasoruce/remote/status/NetworkStatus.kt
@@ -4,6 +4,11 @@ import androidx.lifecycle.MutableLiveData
 
 /**
  * Network Status
+ *
+ * LOADING - Starting to fetch new data
+ * RETRY - Restarting to fetch new data
+ * FAILED - Failed to fetch new data
+ * DONE - Finished the network job
  */
 object NetworkStatus {
 
