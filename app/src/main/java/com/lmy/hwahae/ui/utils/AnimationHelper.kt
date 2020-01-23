6f3af1e6cbda079beecc87c1a953f1a7fb7b4767
mingyoung <tkfkd1212!>
2020-01-23 23:39:43 +0900

    added comments

--- app/src/main/java/com/lmy/hwahae/ui/utils/AnimationHelper.kt
+++ app/src/main/java/com/lmy/hwahae/ui/utils/AnimationHelper.kt
@@ -5,6 +5,9 @@ import androidx.dynamicanimation.animation.DynamicAnimation
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
 
+/**
+ * Spring Animation Helper
+ */
 object AnimationHelper {
     fun getSpringAnimation(view: View, startValue: Float, springForce: SpringForce) : SpringAnimation {
         return SpringAnimation(view, DynamicAnimation.TRANSLATION_Y)
